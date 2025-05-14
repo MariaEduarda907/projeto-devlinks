1db08a068e2e27d3ad0a6815a433dd46ef51c03f
MariaE90 <eduarda9075@gmail.com>
2025-05-14 11:02:32 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+    const html = document.documentElement
+
+  
+
+    html.classList.toggle('light')
+}
+
+//outra forma
+    // if(body.classList.contains('light')){
+    // html.classList.remove('light')
+    // }
+    // else{
+    // html.classList.add('light')
+    // }
+
+
+// . acessa propriedades e funcionalidades
+//let permite mudança de valor
+//const não permite mudança
+
+
+//para troca de imagem
